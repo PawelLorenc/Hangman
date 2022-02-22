a8477217561a549b6494857631c042f6ba23331f
PawelLorenc <pawellorencc@gmail.com>
2022-02-22 19:34:33 +0100

    Class WordToChar added

---
+++ WordToChar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    internal class WordToChar
+    {
+        public static List<char> ConvertStringToChar()
+        {
+            string word = DrawWord.wordToGuess().CapitalCity;
+            List<char> characters = new List<char>();
+            characters.AddRange(word);
+            return characters;
+        }
+    }
+}
