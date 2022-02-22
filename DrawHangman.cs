826d8deb30a67a2fb6af60d51afab394fbfd44f1
PawelLorenc <pawellorencc@gmail.com>
2022-02-22 18:41:49 +0100

    Class DrawHangman added

---
+++ DrawHangman.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    internal class DrawHangman
+    {
+
+        public static void Draw(int chancesLeft)
+        {
+            switch(chancesLeft) {
+                case 5:
+                    Console.WriteLine("\n" +
+                    "  ____   \n" +
+                    " |    |  \n" +
+                    " |       \n" +
+                    " |       \n" +
+                    " |       \n" +
+                    "_|___    \n");
+                    break;
+                case 4:
+                    Console.WriteLine("\n" +
+                    "  ____   \n" +
+                    " |    |  \n" +
+                    " |    O  \n" +
+                    " |       \n" +
+                    " |       \n" +
+                    "_|___    \n");                    
+                    break;
+                case 3:
+                    Console.WriteLine("\n" +
+                    "  ____  \n " +
+                    " |    | \n " +
+                    " |   _O_\n " +
+                    " |      \n " +
+                    "_|___   \n ");
+                    break;
+                case 2:
+                    Console.WriteLine("\n" +
+                    "  ____  \n " +
+                    " |    | \n " +
+                    " |   _O_\n " +
+                    " |    | \n " +
+                    "_|___   \n ");
+                    break;
+                case 1:
+                    Console.WriteLine("\n" +
+                    "  ____  \n " +
+                    " |    | \n " +
+                    " |   _O_\n " +
+                    " |    | \n " +
+                    "_|___/ \\n " +
+                    "THE GAME IS OVER!");
+                    break;
+
+            }
+
+        }
+        
+
+  
+    }
+}
