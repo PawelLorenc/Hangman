66394fd16c48438879b5b3b4f308687278969812
PawelLorenc <pawellorencc@gmail.com>
2022-02-22 19:11:06 +0100

    Class DrawWord added.

---
+++ DrawWord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    internal class DrawWord
+    {
+
+        private static int DrawNumber()
+        {
+            Random random = new Random();
+            int randomNumberToDraw = random.Next(0, LoadWords.TXTfileToList().Count);
+            return randomNumberToDraw;
+        }
+
+        public static LoadWords wordToGuess()
+        {
+           return LoadWords.TXTfileToList().ElementAt(DrawNumber());
+        }       
+
+    }
+}
